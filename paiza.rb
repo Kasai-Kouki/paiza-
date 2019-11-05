c37102c02e49283fa077713a62e5f3d98ef43b86
Kasai-Kouki <kko_7230@yahoo.co.jp>
2019-11-05 12:51:14 +0000

    first commit

---
+++ paiza.rb
@@ -0,0 +1,26 @@
+f = 440
+if f >= 20 && f <= 15000
+  p "yes"
+elsif f > 15000 && f <= 20000
+  p "not sure"
+else
+  p "no"
+end
+
+f = 30000
+if f >= 20 && f <= 15000
+  p "yes"
+elsif f > 15000 && f <= 20000
+  p "not sure"
+else
+  p "no"
+end
+
+f = 18000
+if f >= 20 && f <= 15000
+  p "yes"
+elsif f > 15000 && f <= 20000
+  p "not sure"
+else
+  p "no"
+end
